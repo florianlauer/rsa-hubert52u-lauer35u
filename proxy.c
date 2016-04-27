296fd75dc697d486726a2f21ed8469e53a29f789
Maximatordelabam <maxime.hubert@telecomnancy.net>
2016-04-27 18:05:25 +0200

    Avancement sur le code, et dépar à phi

--- proxy.c
+++ proxy.c
@@ -1,13 +1,13 @@
-#include<stdio.h>
-#include<stdlib.h>
-#include<unistd.h>
-#include<string.h>
-#include<sys/types.h>
-#include<sys/socket.h>
-#include<arpa/inet.h>
-#include<netinet/in.h>
-#include<netdb.h>
-#include<errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <errno.h>
 
 void error(char* msg) {
   perror(msg);
@@ -18,18 +18,18 @@ void error(char* msg) {
 
 int main(int argc,char* argv[]) {
   pid_t pid;
-  struct sockaddr_in addr_in,cli_addr,serv_addr;  // Définition des sockets ?? addr_in ??
+  struct sockaddr_in cli_addr,serv_addr;  // Définition des sockets
   struct hostent* host;
   int sockfd,newsockfd;
 
   if(argc<2)
-  error("./proxy <port_no>");
+    error("veuillez rentrer le numéro de port en argument");
 
 
   /* Initalisation des sockets à zéro */
 
-  bzero((char*)&serv_addr,sizeof(serv_addr));
-  bzero((char*)&cli_addr, sizeof(cli_addr));
+  bzero(&serv_addr, sizeof(serv_addr));
+  bzero(&cli_addr, sizeof(cli_addr));
 
   serv_addr.sin_family=AF_INET;                 // Adresse IPV4
   serv_addr.sin_port=htons(atoi(argv[1]));      // Numéro de port en paramètre de l'executable
@@ -39,6 +39,7 @@ int main(int argc,char* argv[]) {
   /* Ouverture de la socket */
 
   sockfd=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+
   if(sockfd<0) {
     error("Problème lors de l'initialisation");
   }
@@ -48,7 +49,7 @@ int main(int argc,char* argv[]) {
   }
 
 
-  listen(sockfd,50);                            // La socket se met en écoute, file d'attente de 50 connexions
+  listen(sockfd,30);                            // La socket se met en écoute, file d'attente de 30 connexions
   int clilen=sizeof(cli_addr);
 
 
@@ -66,6 +67,7 @@ int main(int argc,char* argv[]) {
   /* Création d'un processus fils */
 
   pid=fork();
+
   if(pid==0) {
     struct sockaddr_in host_addr;
     int flag=0,newsockfd1,n,port=0,i,sockfd1;
@@ -79,86 +81,86 @@ int main(int argc,char* argv[]) {
 
     if(((strncmp(t1,"GET",3)==0))&&((strncmp(t3,"HTTP/1.1",8)==0)||(strncmp(t3,"HTTP/1.0",8)==0))&&(strncmp(t2,"http://",7)==0))
     {
-    strcpy(t1,t2);
+      strcpy(t1,t2);
 
-    flag=0;
+      flag=0;
 
-    for(i=7;i<strlen(t2);i++)
-    {
-    if(t2[i]==':')
-    {
-    flag=1;
-    break;
-    }
-    }
+      for(i=7;i<strlen(t2);i++)
+      {
+        if(t2[i]==':')
+        {
+          flag=1;
+          break;
+        }
+      }
 
-    temp=strtok(t2,"//");
-    if(flag==0)
-    {
-    port=80;
-    temp=strtok(NULL,"/");
-    }
-    else
-    {
-    temp=strtok(NULL,":");
-    }
+      temp=strtok(t2,"//");
+      if(flag==0)
+      {
+        port=80;
+        temp=strtok(NULL,"/");
+      }
+      else
+      {
+        temp=strtok(NULL,":");
+      }
 
-    sprintf(t2,"%s",temp);
-    printf("host = %s",t2);
-    host=gethostbyname(t2);
+      sprintf(t2,"%s",temp);
+      printf("host = %s",t2);
+      host=gethostbyname(t2);
 
-    if(flag==1)
-    {
-    temp=strtok(NULL,"/");
-    port=atoi(temp);
-    }
-
-
-    strcat(t1,"^]");
-    temp=strtok(t1,"//");
-    temp=strtok(NULL,"/");
-    if(temp!=NULL)
-    temp=strtok(NULL,"^]");
-    printf("\npath = %s\nPort = %d\n",temp,port);
+      if(flag==1)
+      {
+        temp=strtok(NULL,"/");
+        port=atoi(temp);
+      }
 
 
-    bzero((char*)&host_addr,sizeof(host_addr));
-    host_addr.sin_port=htons(port);
-    host_addr.sin_family=AF_INET;
-    bcopy((char*)host->h_addr,(char*)&host_addr.sin_addr.s_addr,host->h_length);
+      strcat(t1,"^]");
+      temp=strtok(t1,"//");
+      temp=strtok(NULL,"/");
+      if(temp!=NULL)
+        temp=strtok(NULL,"^]");
+      printf("\npath = %s\nPort = %d\n",temp,port);
 
-    sockfd1=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
-    newsockfd1=connect(sockfd1,(struct sockaddr*)&host_addr,sizeof(struct sockaddr));
-    sprintf(buffer,"\nConnected to %s  IP - %s\n",t2,inet_ntoa(host_addr.sin_addr));
-    if(newsockfd1<0)
-    error("Error in connecting to remote server");
 
-    printf("\n%s\n",buffer);
+      bzero((char*)&host_addr,sizeof(host_addr));
+      host_addr.sin_port=htons(port);
+      host_addr.sin_family=AF_INET;
+      bcopy((char*)host->h_addr,(char*)&host_addr.sin_addr.s_addr,host->h_length);
+
+      sockfd1=socket(AF_INET,SOCK_STREAM,IPPROTO_TCP);
+      newsockfd1=connect(sockfd1,(struct sockaddr*)&host_addr,sizeof(struct sockaddr));
+      sprintf(buffer,"\nConnected to %s  IP - %s\n",t2,inet_ntoa(host_addr.sin_addr));
+      if(newsockfd1<0)
+        error("Error in connecting to remote server");
+
+      printf("\n%s\n",buffer);
     //send(newsockfd,buffer,strlen(buffer),0);
-    bzero((char*)buffer,sizeof(buffer));
-    if(temp!=NULL)
-    sprintf(buffer,"GET /%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n",temp,t3,t2);
-    else
-    sprintf(buffer,"GET / %s\r\nHost: %s\r\nConnection: close\r\n\r\n",t3,t2);
+      bzero((char*)buffer,sizeof(buffer));
+      if(temp!=NULL)
+        sprintf(buffer,"GET /%s %s\r\nHost: %s\r\nConnection: close\r\n\r\n",temp,t3,t2);
+      else
+        sprintf(buffer,"GET / %s\r\nHost: %s\r\nConnection: close\r\n\r\n",t3,t2);
 
 
-    n=send(sockfd1,buffer,strlen(buffer),0);
-    printf("\n%s\n",buffer);
-    if(n<0)
-    error("Error writing to socket");
-    else{
-    do
-    {
-    bzero((char*)buffer,500);
-    n=recv(sockfd1,buffer,500,0);
-    if(!(n<=0))
-    send(newsockfd,buffer,n,0);
-    }while(n>0);
-    }
+      n=send(sockfd1,buffer,strlen(buffer),0);
+      printf("\n%s\n",buffer);
+      if(n<0)
+        error("Error writing to socket");
+      else{
+        do
+        {
+          bzero((char*)buffer,500);
+          n=recv(sockfd1,buffer,500,0);
+          if(!(n<=0))
+            send(newsockfd,buffer,n,0);
+        }while(n>0);
+      }
     }
     else
     {
-    send(newsockfd,"400 : BAD REQUEST\nONLY HTTP REQUESTS ALLOWED",18,0);
+      send(newsockfd,"400 : BAD REQUEST\nONLY HTTP REQUESTS ALLOWED",18,0);
     }
     close(sockfd1);
     close(newsockfd);
@@ -168,8 +170,8 @@ int main(int argc,char* argv[]) {
 
   else
   {
-  close(newsockfd);
-  goto accepting;
+    close(newsockfd);
+    goto accepting;
   }
   return 0;
 }
